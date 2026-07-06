ef4de3352441cab54f8eadb265ed2c11586d3406
krishtyagi-tech <krishtyagi583@gmail.com>
2026-07-06 17:24:19 +0530

    Fix Firebase initialization

--- utils/firebase_admin.py
+++ utils/firebase_admin.py
@@ -1,12 +1,17 @@
+import os
 import firebase_admin
 from firebase_admin import credentials
-import streamlit as st
 
 if not firebase_admin._apps:
 
-    cred = credentials.Certificate(
-        dict(st.secrets["firebase"])
-    )
+    firebase_path = os.getenv("FIREBASE_ADMIN_JSON")
+
+    if firebase_path and os.path.exists(firebase_path):
+        cred = credentials.Certificate(firebase_path)
+    else:
+        cred = credentials.Certificate(
+            os.path.join("secrets", "firebase-admin.json")
+        )
 
     firebase_admin.initialize_app(cred)
 
